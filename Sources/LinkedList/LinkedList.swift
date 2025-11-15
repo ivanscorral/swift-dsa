ebad89162c9e739a8c831bbea20c340e1b37fee7
Ivan Sanchez Corral <ramon.sanchez68@gmail.com>
2025-11-15 16:10:17 +0100

    Full CRUD support for LinkedList and tests

--- Sources/LinkedList/LinkedList.swift
+++ Sources/LinkedList/LinkedList.swift
@@ -49,6 +49,72 @@ final public class LinkedList<Element> {
     public func element(at index: Int) -> Element? {
        _node(at: index)?.value
     }
+    
+    @discardableResult
+    public func removeFirst() -> Element? {
+        guard count > 0, let first = head else { return nil }
+        
+        head = head?.next
+        count -= 1
+        
+        if count == 0 {
+            tail = nil
+        }
+        
+        return first.value
+    }
+    
+    @discardableResult
+    public func removeLast() -> Element? {
+        guard count > 0, let last = tail else { return nil }
+        
+        if count == 1 {
+            head = nil
+            tail = nil
+        } else {
+            var current = head
+            while current?.next !== tail {
+                current = current?.next
+            }
+            tail = current
+        }
+        
+        count -= 1
+        return last.value
+    }
+    
+    @discardableResult
+    public func remove(at index: Int) -> Element? {
+        guard index >= 0, index < count, count > 0 else { return nil }
+                
+        if index == 0 {
+            return removeFirst()
+        } else if index == count - 1 {
+            return removeLast()
+        } else {
+            let previous = _node(at: index - 1)
+            let removedNode = previous?.next
+            previous?.next = removedNode?.next
+            count -= 1
+            return removedNode?.value
+        }
+    }
+    
+    public func insert(at index: Int, value: Element) {
+        guard index >= 0, index <= count else { return } 
+        
+        if index == 0 {
+            prepend(value)
+        } else if index == count {
+            append(value)
+        } else {
+            let newNode = Node(value)
+            let previous = _node(at: index - 1)
+            newNode.next = previous?.next
+            previous?.next = newNode
+            count += 1
+        }
+    }
 }
 
 private extension LinkedList {
--- Tests/LinkedListTests/LinkedListTests.swift
+++ Tests/LinkedListTests/LinkedListTests.swift
@@ -234,4 +234,244 @@ final class LinkedListTests: XCTestCase {
         }
     }
     
+    // MARK: RemoveFirst tests
+    
+    func test_removeFirstOnEmptyList_returnsNil() {
+        let linkedList = LinkedList<Int>()
+        
+        let removedElement = linkedList.removeFirst()
+        
+        XCTAssertNil(removedElement)
+        XCTAssertEqual(linkedList.count, 0)
+        XCTAssertNil(linkedList.head)
+        XCTAssertNil(linkedList.tail)
+    }
+    
+    func test_removeFirstOnSingleElementList_returnsCorrectAndEmptiesList() {
+        let linkedList = LinkedList<Int>()
+        
+        linkedList.append(42)
+        let removedElement = linkedList.removeFirst()
+        
+        XCTAssertEqual(removedElement, 42)
+        XCTAssertEqual(linkedList.count, 0)
+        XCTAssertEqual(linkedList.head, nil)
+        XCTAssertEqual(linkedList.tail, nil)
+    }
+    
+    func test_removeFirstOnMultipleElements_updatesHeadAndCount() {
+        let linkedList = LinkedList<Int>()
+        
+        linkedList.append(1)
+        linkedList.append(2)
+        linkedList.append(3)
+        
+        let removedElement = linkedList.removeFirst()
+        
+        XCTAssertEqual(removedElement, 1)
+        XCTAssertEqual(linkedList.count, 2)
+        XCTAssertEqual(linkedList.head?.value, 2)
+        XCTAssertEqual(linkedList.tail?.value, 3)
+    }
+    
+    // MARK: Remove last tests
+    
+    func test_removeLastOnEmptyList_returnsNil() {
+        let linkedList = LinkedList<Int>()
+        
+        let removedElement = linkedList.removeLast()
+        
+        XCTAssertNil(removedElement)
+        XCTAssertEqual(linkedList.count, 0)
+        XCTAssertNil(linkedList.head)
+        XCTAssertNil(linkedList.tail)
+    }
+    
+    func test_removeLastOnSingleElementList_returnsCorrectAndEmptiesList() {
+        let linkedList = LinkedList<Int>()
+        
+        linkedList.append(42)
+        let removedElement = linkedList.removeLast()
+        
+        XCTAssertEqual(removedElement, 42)
+        XCTAssertEqual(linkedList.count, 0)
+        XCTAssertEqual(linkedList.head, nil)
+        XCTAssertEqual(linkedList.tail, nil)
+    }
+    
+    func test_removeLastOnMultipleElements_updatesTailAndCount() {
+        let linkedList = LinkedList<Int>()
+        
+        linkedList.append(1)
+        linkedList.append(2)
+        linkedList.append(3)
+        
+        let removedElement = linkedList.removeLast()
+        
+        XCTAssertEqual(removedElement, 3)
+        XCTAssertEqual(linkedList.tail?.value, 2)
+        XCTAssertEqual(linkedList.count, 2)
+    }
+    
+    func test_removeLast_structureCorrectAfterSeveralOps() {
+        let linkedList = LinkedList<Int>()
+        
+        linkedList.append(1)
+        linkedList.append(2)
+        linkedList.prepend(0)
+        
+        let removed = linkedList.removeLast()
+        
+        XCTAssertEqual(removed, 2)
+        XCTAssertEqual(linkedList.head?.value, 0)
+        XCTAssertEqual(linkedList.tail?.value, 1)
+    }
+    
+    // MARK: RemoveAt tests
+    
+    func test_removeAtOnEmptyList_returnsNil() {
+        let linkedList = LinkedList<Int>()
+        
+        let removedElement = linkedList.remove(at: 0)
+        
+        XCTAssertNil(removedElement)
+        XCTAssertEqual(linkedList.count, 0)
+    }
+    
+    func test_removeAtValidIndexZero_removesFirst() {
+        let linkedList = LinkedList<Int>()
+        
+        linkedList.append(1)
+        linkedList.append(2)
+        linkedList.append(3)
+        
+        let removedElement = linkedList.remove(at: 0)
+        
+        XCTAssertEqual(removedElement, 1)
+        XCTAssertEqual(linkedList.head?.value, 2)
+        XCTAssertEqual(linkedList.count, 2)
+    }
+    
+    func test_removeAtMiddleIndex_removesCorrectElement() {
+        let linkedList = LinkedList<Int>()
+        
+        linkedList.append(1)
+        linkedList.append(2)
+        linkedList.append(3)
+        linkedList.append(4)
+        
+        let removed = linkedList.remove(at: 2)
+        
+        XCTAssertEqual(removed, 3)
+        for (i, value) in [1, 2, 4].enumerated() {
+            XCTAssertEqual(linkedList.element(at: i), value)
+        }
+        XCTAssertEqual(linkedList.count, 3)
+    }
+    
+    func test_removeAtLastIndex_removesLast() {
+        let linkedList = LinkedList<Int>()
+        
+        linkedList.append(1)
+        linkedList.append(2)
+        linkedList.append(3)
+        
+        let removedElement = linkedList.remove(at: 2)
+        
+        XCTAssertEqual(removedElement, 3)
+        XCTAssertEqual(linkedList.tail?.value, 2)
+        XCTAssertEqual(linkedList.count, 2)
+    }
+    
+    func test_removeAtInvalidIndex_returnsNil() {
+        let linkedList = LinkedList<Int>()
+        linkedList.append(1)
+        
+        var removed = linkedList.remove(at: 10)
+        XCTAssertNil(removed)
+        
+        removed = linkedList.remove(at: -1)
+        XCTAssertNil(removed)
+        
+        removed = linkedList.remove(at: 1)
+        XCTAssertNil(removed)
+        
+    }
+    
+    func test_removeAt_twoElementList_removingFirst_updatesHeadAndTailCorrectly() {
+        let linkedList = LinkedList<Int>()
+        
+        linkedList.append(1)
+        linkedList.append(2)
+        
+        let removedElement = linkedList.remove(at: 0)
+        
+        XCTAssertEqual(removedElement, 1)
+        XCTAssertEqual(linkedList.head?.value, 2)
+        XCTAssertEqual(linkedList.tail?.value, 2)
+        XCTAssertEqual(linkedList.count, 1)
+    }
+    
+    // MARK: Insert at index tests
+    
+    func test_insertAtBeginning_insertsAtHead() {
+        let linkedList = LinkedList<Int>()
+        
+        linkedList.append(1)
+        
+        linkedList.insert(at: 0, value: 0)
+        
+        XCTAssertEqual(linkedList.head?.value, 0)
+        XCTAssertEqual(linkedList.tail?.value, 1)
+        XCTAssertEqual(linkedList.count, 2)
+    }
+    
+    func test_insertAtEnd_insertsAtTail() {
+        let linkedList = LinkedList<Int>()
+        
+        linkedList.append(1)
+        
+        linkedList.insert(at: 1, value: 2)
+        
+        XCTAssertEqual(linkedList.head?.value, 1)
+        XCTAssertEqual(linkedList.tail?.value, 2)
+        XCTAssertEqual(linkedList.count, 2)
+    }
+    
+    func test_insertAtMiddle_insertsCorrectly() {
+        let linkedList = LinkedList<Int>()
+        
+        linkedList.append(1)
+        linkedList.append(3)
+        linkedList.append(4)
+        
+        linkedList.insert(at: 1, value: 2)
+        
+        for (element, val) in zip(linkedList, [1, 2, 3, 4]) {
+            XCTAssertEqual(element, val)
+        }
+    }
+    
+    func test_insertAtOnEmptyList_insertsAtHead() {
+        let linkedList = LinkedList<Int>()
+        
+        linkedList.insert(at: 0, value: 10)
+        
+        XCTAssertEqual(linkedList.head?.value, 10)
+        XCTAssertEqual(linkedList.tail?.value, 10)
+        XCTAssertEqual(linkedList.count, 1)
+    }
+    
+    func test_insertAtInvalidIndex_doesNotInsert() {
+        let linkedList = LinkedList<Int>()
+        
+        linkedList.append(1)
+        
+        linkedList.insert(at: -1, value: 0)
+        XCTAssertEqual(linkedList.count, 1)
+        
+        linkedList.insert(at: 2, value: 2)
+        XCTAssertEqual(linkedList.count, 1)
+    }
+    
 }
